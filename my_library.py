e20215c59f6a16016ab410c22e9908737786776b
ashah2175 <166551250+ashah2175@users.noreply.github.com>
2024-05-27 22:22:32 -0700

    Update my_library.py

--- my_library.py
+++ my_library.py
@@ -4,12 +4,12 @@ def compute_probs(neg,pos):
   p0 = neg/(neg+pos)
   p1 = pos/(neg+pos)
   return [p0,p1]
-
-  def cond_prob(table, evidence, evidence_value, target, target_value):
-    t_subset = up_table_subset(table, target, 'equals', target_value)
-    e_list = up_get_column(t_subset, evidence)
-    p_b_a = sum([1 if v==evidence_value else 0 for v in e_list])/len(e_list)
-    return p_b_a + .01
+  
+def cond_prob(table, evidence, evidence_value, target, target_value):
+  t_subset = up_table_subset(table, target, 'equals', target_value)
+  e_list = up_get_column(t_subset, evidence)
+  p_b_a = sum([1 if v==evidence_value else 0 for v in e_list])/len(e_list)
+  return p_b_a + .01
 
 def cond_probs_product(table, evidence_row, target, target_value):
   evidence_complete = up_zip_lists(table.columns[:-1], evidence_row)
