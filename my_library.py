602b7cb5528863268d7182e61ba8c980b3c47e97
ashah2175 <166551250+ashah2175@users.noreply.github.com>
2024-06-05 00:12:27 -0700

    Update my_library.py

--- my_library.py
+++ my_library.py
@@ -55,3 +55,24 @@ def metrics(zipped_list):
   results = {'Precision': precision, 'Recall': recall, 'F1': f1, 'Accuracy': accuracy}
 
   return results
+
+def try_archs(full_table, target, architectures, thresholds):
+
+  train_table, test_table = up_train_test_split(full_table, target, .4)
+
+  for arch in architectures:
+    all_results = up_neural_net(train_table, test_table, arch, target)
+
+    all_mets = []
+    for threshold in thresholds:
+      all_predictions = [1 if pos>threshold else 0 for neg, pos in all_results]
+      pred_act_list = up_zip_lists(all_predictions, up_get_column(test_table, target))
+
+      mets = metrics(pred_act_list)
+      mets['Threshold'] = threshold
+      all_mets += [mets]
+
+    print(f'Architecture: {arch}')
+    print(up_metrics_table(all_mets))
+
+  return None  #main use is to print out threshold tables, not return anything useful.
